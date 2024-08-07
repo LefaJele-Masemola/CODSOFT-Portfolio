7d294dc12f49d5aaa5e3ff3e675870b91096f601
Lefa <136033739+LefaJele-Masemola@users.noreply.github.com>
2024-08-07 13:22:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const mobileMenu = document.getElementById('mobile-menu');
     const navList = document.querySelector('.nav-list');
 
-    mobileMenu.addEventListener('click', function () {
-        navList.classList.toggle('show');
-    });
+    // Check if elements exist before adding event listeners
+    if (mobileMenu && navList) {
+        mobileMenu.addEventListener('click', function () {
+            navList.classList.toggle('show');
+        });
+    }
 });
